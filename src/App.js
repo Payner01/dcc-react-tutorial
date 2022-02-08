590929db958e28e18825d1f79c3c079fd5567cc8
Bryce Payne <bryce.payne.97@gmail.com>
2022-02-08 15:12:33 -0600

    added add entry form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
-
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 
 function App() {
 
 const [entries, setEntries] = useState([{weight: 175, date: '2/8/2022'}, {weight: 176, date: '2/9/2020'}])
 
+function addNewEntry(entry){
+
+  let tempEntries = [...entries, entry];
+
+  setEntries(tempEntries);
+
+}
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
+
     </div>
   );
 }
